e982d0da5f2ed242e2f43aa00db008aea8195ef8
panchopinto <69988761+panchopinto@users.noreply.github.com>
2025-09-07 14:16:17 -0300

    Delete js directory

--- js/guard_resp2.js
+++
@@ -1,5 +0,0 @@
-// Redirige si no se encuentra un token simple de acceso (opcional)
-document.addEventListener('DOMContentLoaded', ()=>{
-  const ok = localStorage.getItem('auth') === 'ok';
-  // if(!ok){ location.href = 'index.html'; }
-});
